3fef19951eb0aa3f2a96860097eaca58724c3020
JoshMJ37 <jmjaffe37@gatech.edu>
2020-04-03 15:33:39 -0400

    put all in 1 file

--- tsp-3510.py
+++ tsp-3510.py
@@ -1,10 +1,33 @@
 import numpy
 import scipy
 from scipy.spatial.distance import pdist
+import sys
 
+inputFile = sys.argv[1]
+outputFile = sys.argv[2]
+time = sys.argv[3]
+arr = []
+cost = 29
+file = open(inputFile, 'r')
+for line in file:
+    fields = line.split(" ")
+    arr.append((float(fields[1]),float(fields[2])))
+file.close()
+
+file = open(outputFile, 'w')
+file.write(str(cost)+'\n')
+file.write(str(arr))
+file.close()
+
+print(time, inputFile, outputFile)
+# print(arr)
+
+tsp(inputFile, outputFile, time)
 
 def tsp(input_coor, output-tour, time):
-    read(input_coor)
+    print('function goes here')
+
+
 
 def memoize(f):
     memo = {}
@@ -32,7 +55,3 @@ def get_pdist(n1, n2, arr, numNodes):
 
     return arr[base + maxNode]
 
-
-# if __name__ == "__main__":
-#     tsp()ls
-#
